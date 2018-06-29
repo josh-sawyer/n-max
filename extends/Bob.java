bb81b7e370cb777fdedbd018fe31ba533726a4a0
jsawy <12228045+jsawy@users.noreply.github.com>
2018-06-28 20:16:17 -0400

    Update Bob.java

--- extends/Bob.java
+++ extends/Bob.java
@@ -3,7 +3,10 @@ package example;
 public class Bob extends Person {
 	
 	public void speak() {
-		super.speak();
-		System.out.println("My Name is Bob.");
+		super.speak(); // super accesses the Parent Object the Bob is extending
+			       // and calles its speak method.
+
+		System.out.println("My Name is Bob."); // following the execution of the super
+						       // speak method call then prints message.
 	}
 }
