aa00287139c094ba3991012624cc86cb66d9c275
jsawy <12228045+jsawy@users.noreply.github.com>
2018-06-28 20:19:10 -0400

    Update Person.java

--- extends/Person.java
+++ extends/Person.java
@@ -1,6 +1,6 @@
 package example;
 
-public class Person {
+public class Person { // This is the base Person class that will be extended
 	
 	public void speak() {
 		System.out.print("Person says ");
