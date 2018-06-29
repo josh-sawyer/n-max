5f648f2c3b288feb7d06de65cfb037833de7f96a
jsawy <12228045+jsawy@users.noreply.github.com>
2018-06-28 20:14:40 -0400

    Update MyExample.java

--- extends/MyExample.java
+++ extends/MyExample.java
@@ -3,12 +3,21 @@ package example;
 public class MyExample {
 
 	public static void main(String[] args) {
-		Person[] people = new Person[2];
-		people[0] = new Bob();
-		people[1] = new Fred();
-		for(Person person : people) {
-			System.out.println(person.getClass().getName());
-			person.speak();
+		Person[] people = new Person[2]; // Create a list of Objects of type Person.
+
+		people[0] = new Bob(); // Create a new Bob Object who is a Person Object
+		                       // and assign it to index 0 of the Person array.
+
+		people[1] = new Fred(); // Create a new Fred Object who is a Person Object
+		                        // and assign it to index 1 of the Person array.
+
+		for(Person person : people) { // Loop through the Person array.
+
+			System.out.println(person.getClass().getName()); // Print the Object type of
+									 // the person.
+
+			person.speak(); // Call the speak method of each Person Object
+					// which is overwritten by the extended SubClass.
 		}
 	}
 
